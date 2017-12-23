7af81008c310594890202664be5e5c3426a0aa1b
Owocki <ksowocki@gmail.com>
2017-12-22 17:05:41 -0700

    twiter card

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,14 +86,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation

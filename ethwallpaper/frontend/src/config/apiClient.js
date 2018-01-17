6a49c830ba5fe333214c4391210728a6f2f6f724
Eswara Sai <eswarasai.v@gmail.com>
2018-01-18 02:03:47 +0530

    Initial commit of LineChart for gas price vs confirmation time

--- ethwallpaper/frontend/src/config/apiClient.js
+++ ethwallpaper/frontend/src/config/apiClient.js
@@ -11,3 +11,5 @@ const apiClient = axios.create({
 export default apiClient;
 
 export const WALLPAPERS = 'wallpapers/';
+
+export const chartDataUrl = 'https://ethgasstation.info/json/predictTable.json';
